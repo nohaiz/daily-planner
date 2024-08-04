b4637d3d29dc31da712b99f754807a10e4b06bc9
nohaiz <nohaiznazier@gmail.com>
2024-08-04 12:55:01 +0300

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 function App() {
 
+  const dailyTasks = [
+    {name: 'Morning Exercise', completed: true},
+    {name: 'Read a Book', completed: false},
+    {name: 'Work on Project', completed: true},
+    {name: 'Grocery Shopping', completed: false},
+    {name: 'Prepare Dinner', completed: true},
+    {name: 'Evening Relaxation', completed: true}
+  ];
+  
   return (
     <>
-      
+      <h1>My Tasks</h1>
+      <ul>
+        {dailyTasks.map((task, index) => 
+          <li key={index} className={task.completed? 'completed': 'incompleted'}>
+            {task.name};
+          </li>
+        )}
+      </ul>
     </>
   )
 }
